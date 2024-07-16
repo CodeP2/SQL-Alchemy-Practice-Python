a328a550f49df260ef33da827631184aff44010e
CodeP2 <radomski.maciek@yahoo.com>
2024-07-16 14:58:42 +0200

    added return to get values out of query

--- entries_by_x.py
+++ entries_by_x.py
@@ -2,27 +2,31 @@ import books_holder
 
 
 def Index_list():
-    return [item[0] for item in books_holder.session.query(books_holder.Books.id)]
+    return get_entry_values_by(getattr(books_holder.Books, "id"))
 
 
 def author_name_list():
-    author_names = collect_entries("author")
+    author_names, column_names = collect_entries("author")
     stop_entries(author_names)
+    return get_entry_values_by(column_names)
 
 
 def title_list():
-    title_names = collect_entries("title")
+    title_names, column_names = collect_entries("title")
     stop_entries(title_names)
+    return get_entry_values_by(column_names)
 
 
 def publish_date_list():
-    publish_dates = collect_entries("published")
+    publish_dates, column_names = collect_entries("published")
     stop_entries(publish_dates)
+    return get_entry_values_by(column_names)
 
 
 def price_list():
-    prices = collect_entries("price")
+    prices, column_names = collect_entries("price")
     stop_entries(prices)
+    return get_entry_values_by(column_names)
 
 
 def collect_entries(column_name):
@@ -30,7 +34,7 @@ def collect_entries(column_name):
     column = getattr(books_holder.Books, column_name)
     for entry in books_holder.session.query(column):
         empty_list.append(entry)
-    return empty_list
+    return empty_list, column
 
 
 def stop_entries(entry):
@@ -41,3 +45,7 @@ def stop_entries(entry):
         if counter == 5:
             input("press enter to continue...")
             counter = 0
+
+
+def get_entry_values_by(column_name):
+    return [item[0] for item in books_holder.session.query(column_name)]
